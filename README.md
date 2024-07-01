0a3bdf3212a38b26a180fc5cfc533e92d59a78a6
Link-Yundi <7795351+link_yundi@user.noreply.gitee.com>
2024-07-01 10:26:19 +0800

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # ycat
 本地文件数据库，支持所有格式
+
+### 安装
+```shell
+pip install ycat
+```
+
+### 使用示例
+```python
+import ycat
+
+data = [1, 2, 3]
+table_name = 'a.b.c.test'
+
+# 存储数据->catdb/a/b/c/test
+ycat.put(data, table_name)
+# 读取
+ycat.get(table_name)
+# 删除
+ycat.delete(table_name)
+# 是否存在某张表
+ycat.has(table_name)
+
+# 默认db: {pwd}/catdb
+# 改变数据库
+ycat.connect('/your/db/path')
+```
---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+---------------------------------------------
+Created on 2024/7/1 10:20
+@author: ZhangYundi
+@email: yundi.xxii@outlook.com
+---------------------------------------------
+"""
+
+VERSION = '1.0.0'
+from setuptools import setup, find_packages
+
+setup(
+    name='ycat',
+    version=VERSION,
+    py_modules=['ycat'],
+    install_requires=['filelock',
+                      'joblib'],
+
+    author='ZhangYundi',
+    author_email='yundi.xxii@outlook.com',
+    packages=find_packages(include=['ycat', 'ycat.*']),
+    description='Local File Database',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/link-yundi/ycat',
+
+    scripts=[],
+    package_data={},
+)
